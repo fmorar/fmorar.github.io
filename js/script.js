cfef87863384c7b8cd2768ee20d4a63e2a14d38c
fabian <fabianmorar1223@gamial.com>
2015-04-14 12:41:13 -0600

    detalles finales js medio funciona

--- js/script.js
+++ js/script.js
@@ -9,31 +9,16 @@ var appPortafolio = {};
                 var works =" ";
                 
                 for(var i=0; i < data.length; i++){
-                   works += '<div id="'+data[i].categories[4].value+'" class="work"><div class="overlay"><img "class="view-work" src="'+data[i].categories[1].value+'" alt="'+data[i].categories[3].value+'"/></div></div>';
-                }   
+                   works += '<div class="work"><div class="overlay"><a id="'+data[i].categories[4].value+'" href="work-details.html"><img "class="view-work" src="'+data[i].categories[1].value+'" alt="'+data[i].categories[3].value+'"/</a></div></div>';
+                } // for
                 document.getElementById('content-work').innerHTML=works;
 
- 							var StoreTasks = document.querySelectorAll('.work');
+            }// printworks
 
- 							for(var k=0; k < StoreTasks.length; k++){
- 								StoreTasks[k].onclick = function(){
- 									var number = this.id;
- 									console.log(number)
- 									var details = "";
- 									details += '<span class="prevpost"><a href="#">prev</a></span><span class="nextpost"><a href="#">next</a></span><span class="close"><a href="#">close</a></span><h4>'+data[number].categories[0].value+'</h4><p>'+data[number].categories[1].value+'</p><img src="'+data[k].categories[5].value+'" alt="imagen de proyecto">';
- 								}	
- 								document.getElementById('content-details').innerHTML= details;
- 								//document.getElementById('content-details').style.display='block';
- 							}
+	PrintWorks()     
 
-			 	
-            }
-	PrintWorks()            
- }
+ }// appPortafolio.proyectos
 
- // imprimir detalles 
-
- 	
 
 briefcase.getJSON({leftColumnTitle:"proyectos"}, appPortafolio.proyectos);
 
